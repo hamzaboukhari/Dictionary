e87910510f81331500d42250e18bed9d5c26a478
Hamza Boukhari <hb2212@fusion19.doc.ic.ac.uk>
2013-03-05 15:15:51 +0000

    linked list almost complete

--- src/dictionary/OrderedLinkedList.java
+++ src/dictionary/OrderedLinkedList.java
@@ -10,5 +10,127 @@ import java.util.NoSuchElementException;
  */
 public class OrderedLinkedList<K extends Comparable<? super K>, V> implements
         Dictionary<K, V> {
-    // TODO
+	
+	private OrderedLinkedListEntry<K, V> head = null;
+	private int numElems = 0;
+	
+	public void createOrderedList() {
+		
+	}
+
+	@Override
+	public Iterator<DictionaryEntry<K, V>> iterator() {
+		return new ListIterator<K, V>();
+	}
+	
+	public class ListIterator<K, V> implements Iterator<dictionary.DictionaryEntry<K, V>> {
+
+		private OrderedLinkedListEntry<K, V> current;
+		
+		private ListIterator<K, V>() {
+			current = head;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+		
+		@Override
+		public DictionaryEntry<K, V> next() {
+			if (current == null) {
+				return null;
+			} else {
+				result = current.getElem();
+				current = current.getNext();
+				return result;
+			}
+		
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+	@Override
+	public int size() {
+		return numElems;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return numElems == 0;
+	}
+
+	@Override
+	public V get(K key) throws NoSuchElementException {
+		OrderedLinkedListEntry<K, V> prev = head;
+		if (prev == null) {
+			return null;
+		} else if (prev.getKey().equals(key)) {
+			return prev.getValue();
+		} else {
+			OrderedLinkedListEntry<K, V> curr = prev.getNext();
+			while (curr != null && curr.getKey().compareTo(key) == -1) {
+				prev = curr;
+				curr = curr.getNext();	
+			}
+			if (curr != null && curr.getKey().compareTo(key) == 0) {
+				return curr.getValue();
+			}
+		}
+		return null;
+	}
+	
+	private OrderedLinkedListEntry<K, V> findPrev(K searchKey) {
+		OrderedLinkedListEntry<K, V> prev = head;
+		if (prev != null && prev.getKey().compareTo(searchKey) < 0) {
+			OrderedLinkedListEntry<K, V> curr = prev.getNext();
+			while (curr != null && curr.getKey().compareTo(searchKey) <= 0) {
+				prev = curr;
+				curr = curr.getNext();
+			}
+		}
+		return prev;
+	}
+
+	@Override
+	public void put(K key, V value) {
+		OrderedLinkedListEntry<K, V> prev = findPrev(key);
+		if (prev == null) {
+			OrderedLinkedListEntry<K, V> newNode = new OrderedLinkedListEntry<K, V>(key, value);
+			newNode.setNext(head);
+			numElems++;
+		} else if (prev.getKey().equals(key)) {
+			prev.setValue(value);
+		} else if (prev.getKey().compareTo(key) == -1) {
+			OrderedLinkedListEntry<K, V> newNode = new OrderedLinkedListEntry<K, V>(key, value);
+			numElems++;
+			newNode.setNext(prev.getNext());
+			prev.setNext(newNode);
+	    } else {
+	    	OrderedLinkedListEntry<K, V> newNode = new OrderedLinkedListEntry<K, V>(key, value);
+	    	numElems++;
+			newNode.setNext(head);
+	    }
+		
+	}
+
+	@Override
+	public void remove(K key) throws NoSuchElementException {
+		OrderedLinkedListEntry<K, V> prev = findPrev(key);
+		if (prev == null) {
+			head = head.getNext();
+		} else if (prev.getNext().getKey().equals(key)) {
+	    	findPrev(prev.getKey()).setNext(prev.getNext());
+	    } 
+	}
+
+	@Override
+	public void clear() {
+		head = null;
+	}
+
 }
--- src/dictionary/OrderedLinkedListEntry.java
+++ src/dictionary/OrderedLinkedListEntry.java
@@ -2,5 +2,35 @@ package dictionary;
 
 
 public class OrderedLinkedListEntry<K, V> implements DictionaryEntry<K, V> {
-    // TODO
+
+	private K key;
+	private V value;
+	private OrderedLinkedListEntry<K, V> next;
+	
+	public OrderedLinkedListEntry(K k, V v) {
+		key = k;
+		value = v;
+	}
+	
+	@Override
+	public K getKey() {
+		return this.key;
+	}
+
+	@Override
+	public V getValue() {
+		return this.value;
+	}
+	
+	public void setValue(V newValue) {
+		this.value = newValue;
+	}
+
+	public void setNext(OrderedLinkedListEntry<K, V> node) {
+		this.next = node;
+	}
+	
+	public OrderedLinkedListEntry<K, V> getNext() {
+		return this.next;
+	}
 }
